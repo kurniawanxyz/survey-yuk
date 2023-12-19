47b6b07723141c562f8608f397dd54bb1284f44e
21jefri <muhammadjefridnc@gmail.com>
2023-12-20 00:52:47 +0700

    up

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,9 +1,93 @@
-<form action="{{ route('forgot-password') }}" method="POST">
-    @csrf
-    <label for="email">Masukkan Email Anda</label>
-    <input type="email" name="email" id="email" value="{{ old('email') }}">
-    @error('email')
-        <p style="color:red">{{ $message }}</p>
-    @enderror
-    <button type="submit">Check</button>
-</form>
+<!DOCTYPE html>
+<html lang="en">
+
+<!-- Mirrored from demos.adminmart.com/premium/bootstrap/modernize-bootstrap/package/html/main/authentication-forgot-password.html by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 06 Jul 2023 02:01:04 GMT -->
+
+<head>
+    <!--  Title -->
+    <title>Mordenize</title>
+    <!--  Required Meta Tag -->
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="handheldfriendly" content="true" />
+    <meta name="MobileOptimized" content="width" />
+    <meta name="description" content="Mordenize" />
+    <meta name="author" content="" />
+    <meta name="keywords" content="Mordenize" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <!--  Favicon -->
+    <link rel="shortcut icon" type="image/png" href="{{ asset('favicon.ico') }}" />
+    <!-- Core Css -->
+    <link id="themeColors" rel="stylesheet" href="{{ asset('dist/css/style.min.css') }}" />
+</head>
+
+<body>
+    <!-- Preloader -->
+    <div class="preloader">
+        <img src="{{ asset('logo.png') }}" alt="loader" class="lds-ripple img-fluid" />
+    </div>
+    <!--  Body Wrapper -->
+    <div class="page-wrapper" id="main-wrapper" data-layout="vertical" data-sidebartype="full"
+        data-sidebar-position="fixed" data-header-position="fixed">
+        <div class="position-relative overflow-hidden radial-gradient min-vh-100">
+            <div class="position-relative z-index-5">
+                <div class="row">
+                    <div class="col-lg-6 col-xl-8 col-xxl-9">
+                        <div class="d-none d-lg-flex align-items-center justify-content-center"
+                            style="height: calc(100vh - 80px);">
+                            <img src="https://demos.adminmart.com/premium/bootstrap/modernize-bootstrap/package/dist/images/backgrounds/login-security.svg"
+                                alt="" class="img-fluid" width="500">
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-xl-4 col-xxl-3">
+                        <div class="card mb-0 shadow-none rounded-0 min-vh-100 h-100">
+                            <div class="d-flex align-items-center w-100 h-100">
+                                <div class="card-body">
+                                    <div class="mb-5">
+                                        <h2 class="fw-bolder fs-7 mb-3">Lupa kata sandi ?</h2>
+                                        <p class="mb-0 ">
+                                            Silakan masukkan alamat email yang terkait dengan akun Anda, dan kami
+                                            akan mengirimkan tautan untuk mereset kata sandi Anda.
+                                        </p>
+                                    </div>
+                                        <form action="{{ route('forgot-password') }}" method="POST">
+                                            <div class="mb-3">
+                                                @csrf
+                                                <label for="email" class="form-label">Masukkan Email Anda</label>
+                                                <input type="email" class="form-control" name="email" id="email"
+                                                    value="{{ old('email') }}">
+                                                @error('email')
+                                                    <p style="color:red">{{ $message }}</p>
+                                                @enderror
+                                            </div>
+                                            <button type="submit"
+                                                class="btn btn-primary w-100 py-8 mb-3">Check</button>
+                                        </form>
+                                        <a href="login" class="btn btn-light-primary text-primary w-100 py-8">Kembali
+                                            Ke Halaman Login</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+    <!--  Import Js Files -->
+    <script src="../../dist/libs/jquery/dist/jquery.min.js"></script>
+    <script src="../../dist/libs/simplebar/dist/simplebar.min.js"></script>
+    <script src="../../dist/libs/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+    <!--  core files -->
+    <script src="../../dist/js/app.min.js"></script>
+    <script src="../../dist/js/app.init.js"></script>
+    <script src="../../dist/js/app-style-switcher.js"></script>
+    <script src="../../dist/js/sidebarmenu.js"></script>
+
+    <script src="../../dist/js/custom.js"></script>
+</body>
+
+<!-- Mirrored from demos.adminmart.com/premium/bootstrap/modernize-bootstrap/package/html/main/authentication-forgot-password.html by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 06 Jul 2023 02:01:04 GMT -->
+
+</html>
